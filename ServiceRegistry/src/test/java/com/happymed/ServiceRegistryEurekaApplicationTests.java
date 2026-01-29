39dff51da4a00883e0284e9850255c1db285991b
chandra-cs <chandrasekharjena110@gmail.com>
2026-01-29 13:09:26 +0530

    eureka-service-registry

---
+++ ServiceRegistry/src/test/java/com/happymed/ServiceRegistryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happymed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
